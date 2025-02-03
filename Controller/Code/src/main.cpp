6461d8fbfc00d29a2c67d606d0299e3717bb613e
Carl Raymond <carl@orcharddev.com>
2025-02-02 19:54:24 -0500

    Change pin map, increase scan rate

--- Controller/Code/src/main.cpp
+++ Controller/Code/src/main.cpp
@@ -4,15 +4,16 @@ uint16_t ONTIME_SHORT = 8;
 uint16_t ONTIME_LONG = 50;
 
 // Pin assignments
-const int8_t PIN_BELL = 2;      // Bell, active LOW
+const int8_t PIN_LED_A = 2;     // IR LED1, active HIGH
+const int8_t PIN_LED_B = 3;     // IR LED2, active HIGH
+const int8_t PIN_LED_C = 4;     // IR LED3, active HIGH
+const int8_t PIN_LED_D = 5;     // IR LED_D, active HIGH
+const int8_t PIN_DET1 = 6;      // Detector 1, active LOW
+const int8_t PIN_DET2 = 7;      // Detector 2, active LOW
+const int8_t PIN_BELL = 13;     // Bell, active HIGH
 const int8_t PIN_38KHz = 9;     // 38KHz
-const int8_t PIN_LED_A = 10;    // IR LED1, active HIGH
-const int8_t PIN_LED_B = 11;    // IR LED2, active HIGH
-const int8_t PIN_LED_C = 12;    // IR LED3, active HIGH
-const int8_t PIN_LED_D = 13;    // IR LED_D, active HIGH
 
-
-// Cycles 1 -> 2 -> 3 -> 4
+// Cycles 1 -> 2 -> 3 -> 4 at 400Hz
 volatile int8_t activeLed = 1;
 
 void setup() {
@@ -20,7 +21,7 @@ void setup() {
   // Configure Timer1 for 38,000Hz square wave on pin 9 (PortB 1)
   DDRB |= (1 << DDB1);    // Output on pin 9
   ICR1 = 420;             // Set top
-  OCR1A = 210;            // Set compare
+  OCR1A = 105;            // Set compare
   
   // Timer 1 mode bits:
   // CS12=0, CS11=0, CS10=1: Prescaler=1
@@ -35,8 +36,8 @@ void setup() {
   TCCR2A = (1 << WGM21);
   TCCR2B = (1 << CS22) | (1 << CS21) | (1 << CS20);
 
-  // Set compare match register for 100Hz
-  OCR2A = (16000000 / 1024) / 100 - 1; 
+  // Set compare match register for 400Hz
+  OCR2A = (16000000 / 1024) / 400 - 1; 
 
   // Enable Timer 2 output compare interrupt
   TIMSK2 |= (1 << OCIE2A);
@@ -46,38 +47,64 @@ void setup() {
 
   // Configure GPIO
   pinMode(PIN_BELL, OUTPUT);
-  digitalWrite(PIN_BELL, HIGH);
+  digitalWrite(PIN_BELL, LOW);
   pinMode(PIN_LED_A, OUTPUT);
-  digitalWrite(PIN_LED_A, LOW);
+  digitalWrite(PIN_LED_A, HIGH);
   pinMode(PIN_LED_B, OUTPUT);
   digitalWrite(PIN_LED_B, LOW);
   pinMode(PIN_LED_C, OUTPUT);
   digitalWrite(PIN_LED_C, LOW);
   pinMode(PIN_LED_D, OUTPUT);
   digitalWrite(PIN_LED_D, LOW);
-  
+  pinMode(PIN_DET1, INPUT);
+  pinMode(PIN_DET2, INPUT);
 }
 
+// Invoked at 400Hz
 ISR(TIMER2_COMPA_vect) {
+
+  static volatile uint8_t ontime = 0;
+
+  // Read detectors (Port D bits 6 and 7). If any activity, reset
+  // the timeout counter.
+  int8_t det = PIND & 0b11000000;
+  if (det != 0b11000000) {
+    ontime = 200;  // 0.5 seconds @ 400Hz
+  }
+
+  // Indicate output (Pin 13 = PORTB bit 5, active HIGH)
+  if (ontime > 0) {
+    PORTB |= (1 << 5);
+    ontime--;
+  }
+  else {
+    PORTB &= ~(1 << 5);
+  }
+
+  // LEDs on PORTD
   switch(activeLed) {
     case 1:
-      digitalWrite(PIN_LED_A, LOW);
-      digitalWrite(PIN_LED_B, HIGH);
+      // Bit 3 on, bit 2 off
+      PORTD |= (1 << 3);
+      PORTD &= ~(1 << 2);
       activeLed = 2;
       break;
     case 2:
-      digitalWrite(PIN_LED_B, LOW);
-      digitalWrite(PIN_LED_C, HIGH);
+      // Bit 4 on, bit 3 off
+      PORTD |= (1 << 4);
+      PORTD &= ~(1 << 3);
       activeLed = 3;
       break;
     case 3:
-      digitalWrite(PIN_LED_C, LOW);
-      digitalWrite(PIN_LED_D, HIGH);
+      // Bit 5 on, bit 4 off
+      PORTD |= (1 << 5);
+      PORTD &= ~(1 << 4);
       activeLed = 4;
       break;
     default:
-      digitalWrite(PIN_LED_D, LOW);
-      digitalWrite(PIN_LED_A, HIGH);
+      // Bit 2 on, bit 5 off
+      PORTD |= (1 << 2);
+      PORTD &= ~(1 << 5);
       activeLed = 1;
       break;
   }
